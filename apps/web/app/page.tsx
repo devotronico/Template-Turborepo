6ce6cad3f3c133ba287c419d9ac168cb37817444
Daniele Manzi <dmanzi83@gmail.com>
2024-03-02 16:11:48 +0100

    fix app docs

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@ import { Button, Card } from "@repo/ui";
 export default function Page(): JSX.Element {
   return (
     <main className="w-full h-screen bg-green-300 p-20 space-y-20">
-      <h1>CIAO</h1>
+      <h1>App Web</h1>
       <Button appName="web">Click me!</Button>
       <Card title="Titolo" href="https://example.com/">
         <span>card di prova</span>
